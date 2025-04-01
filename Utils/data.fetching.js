c150d5bd168c92375633e3cb82c3d3ed64093116
kozadireza <hanna.zhamoida1993@gmail.com>
2025-04-01 14:29:02 +0100

    Ticket 7 was done

--- Utils/data.fetching.js
+++ Utils/data.fetching.js
@@ -1,29 +1,44 @@
 import axios from "axios";
 
-const url = `https://nc-news-b8jg.onrender.com/api/`;
+const apiRequest = axios.create({
+  baseURL: "https://nc-news-b8jg.onrender.com/api/",
+});
 
 const getUserByName = (userName) => {
-  return axios.get(url + `users/${userName}`).then(({ data }) => {
+  return apiRequest.get(`users/${userName}`).then(({ data }) => {
     return data.user;
   });
 };
 
 const getArticles = () => {
-  return axios.get(url + `articles`).then(({ data }) => {
+  return apiRequest.get(`articles`).then(({ data }) => {
     return data.articles;
   });
 };
 const getArticleById = (article_id) => {
-  return axios.get(url + `articles/${article_id}`).then(({ data }) => {
+  return apiRequest.get(`articles/${article_id}`).then(({ data }) => {
     return data.article;
   });
 };
+const patchArticleById = (article_id, updatingInf) => {
+  return apiRequest
+    .patch(`articles/${article_id}`, updatingInf)
+    .then(({ data }) => {
+      console.log(data);
+      return data.article;
+    });
+};
 
 const getComments = (article_id) => {
-  return axios.get(url + `articles/${article_id}/comments`).then(({ data }) => {
-    console.log(data);
+  return apiRequest.get(`articles/${article_id}/comments`).then(({ data }) => {
     return data.comments;
   });
 };
-const functions = { getUserByName, getArticles, getArticleById, getComments };
+const functions = {
+  getUserByName,
+  getArticles,
+  getArticleById,
+  getComments,
+  patchArticleById,
+};
 export default functions;
